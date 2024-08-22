e67130c5f68b80993189bef87e23195b7e1943ee
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-08-22 21:28:05 +0200

    Added SerienStream- & DownloadClient

---
+++ SerienStreamAPI/Client/DownloadClient.cs
@@ -0,0 +1,149 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using SerienStreamAPI.Internal;
+using System.Text.RegularExpressions;
+using System.Text;
+using SerienStreamAPI.Exceptions;
+
+namespace SerienStreamAPI.Client;
+
+public partial class DownloadClient
+{
+    [GeneratedRegex(@"'hls':\s*'(.*?)'")]
+    private static partial Regex VoeStreamUrlRegex();
+
+    [GeneratedRegex("document\\.getElementById\\('norobotlink'\\)\\.innerHTML = (.+);")]
+    private static partial Regex StreamtapeNoRobotRegex();
+
+    [GeneratedRegex("token=([^&']+)")]
+    private static partial Regex StreamtapeTokenRegex();
+    
+    [GeneratedRegex(@"/pass_md5/([^/]+/[^']+)")]
+    private static partial Regex DoodstreamPassMd5Regex();
+
+
+    private static readonly Random random = new();
+    private static readonly string randomCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    private static string RandomString(
+        int lenght = 10)
+    {
+        string result = "";
+        for (int i = 0; i < lenght; i++)
+        {
+            int index = random.Next(randomCharacters.Length);
+            result += randomCharacters[index];
+        }
+
+        return result;
+    }
+
+
+    readonly ILogger<DownloadClient>? logger;
+
+    readonly RequestHelper requestHelper;
+
+    public DownloadClient(
+        ILogger<DownloadClient>? logger = null)
+    {
+        this.logger = logger;
+
+        this.requestHelper = new(false, logger);
+
+        logger?.LogInformation("[DownloadClient-.ctor] DownloadClient has been inizialized.");
+    }
+
+
+    async Task<HtmlNode> GetHtmlRootAsync(
+        string url,
+        CancellationToken cancellationToken = default)
+    {
+        logger?.LogInformation("[SerienStreamClient-GetHtmlRootAsync] Getting HTML document: {url}...", url);
+        string webContent = await requestHelper.GetAndValidateAsync(url, null, null, cancellationToken);
+
+        HtmlDocument document = new();
+        document.LoadHtml(webContent);
+
+        return document.DocumentNode;
+    }
+
+
+    public async Task<string> GetVoeStreamUrlAsync(
+        string videoUrl,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync(videoUrl, cancellationToken);
+
+        // Extract stream url from video
+        logger?.LogInformation("[DownloadClient-GetVoeStreamUrlAsync] Extracting voe stream url from video: {videoUrl}...", videoUrl);
+
+        string js = root.SelectSingleNodeText("//script[contains(text(), 'var sources')]");
+
+        Match match = VoeStreamUrlRegex().Match(js);
+        if (!match.Success)
+            throw new UrlExtractionFailedException(videoUrl);
+
+        byte[] encodedData = Convert.FromBase64String(match.Groups[1].Value);
+        return Encoding.UTF8.GetString(encodedData);
+    }
+
+    public async Task<string> GetStreamtapeStreamUrlAsync(
+        string videoUrl,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync(videoUrl.Replace("/e/", "/v/"), cancellationToken);
+
+        // Extract stream url from video
+        logger?.LogInformation("[DownloadClient-GetStreamtapeStreamUrlAsync] Extracting voe stream url from video: {videoUrl}...", videoUrl);
+
+        Match norobotLinkMatch = StreamtapeNoRobotRegex().Match(root.InnerHtml);
+        if (!norobotLinkMatch.Success)
+            throw new UrlExtractionFailedException(videoUrl);
+
+        Match tokenMatch = StreamtapeTokenRegex().Match(norobotLinkMatch.Groups[1].Value);
+        if (!tokenMatch.Success)
+            throw new UrlExtractionFailedException(videoUrl);
+
+        string hostUrl = root.SelectSingleNodeText("//div[@id='ideoooolink' and @style='display:none;']");
+        return $"https://{hostUrl}&token={tokenMatch.Groups[1].Value}&dl=1s";
+    }
+
+    public async Task<string> GetDoodstreamStreamUrlAsync(
+        string videoUrl,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync(videoUrl, cancellationToken);
+
+        // Extract stream url from video
+        logger?.LogInformation("[DownloadClient-GetDoodstreamStreamUrlAsync] Extracting voe stream url from video: {videoUrl}...", videoUrl);
+
+        string js = root.SelectSingleNodeText("//script[contains(text(), '/pass_md5/')]");
+
+        Match match = DoodstreamPassMd5Regex().Match(js);
+        if (!match.Success)
+            throw new UrlExtractionFailedException(videoUrl);
+
+        string passMd5 = match.Groups[1].Value;
+        string streamUrl = await requestHelper.GetAndValidateAsync($"https://dood.li/pass_md5/{passMd5}", null, [("Referer", videoUrl)], cancellationToken);
+        long expiry = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+        return $"{streamUrl}{RandomString(10)}?token={passMd5}&expiry={expiry}";
+    }
+
+    public async Task<string> GetVidozaStreamUrlAsync(
+        string videoUrl,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync(videoUrl, cancellationToken);
+
+        // Extract stream url from video
+        logger?.LogInformation("[DownloadClient-GetVoeStreamUrlAsync] Extracting voe stream url from video: {videoUrl}...", videoUrl);
+
+        return root.SelectSingleNodeAttribute("//video[@id='player']/source", "src");
+    }
+
+}
---
+++ SerienStreamAPI/Client/SerienStreamClient.cs
@@ -0,0 +1,177 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using SerienStreamAPI.Enums;
+using SerienStreamAPI.Exceptions;
+using SerienStreamAPI.Internal;
+using SerienStreamAPI.Models;
+using System.Text.RegularExpressions;
+
+namespace SerienStreamAPI.Client;
+
+public partial class SerienStreamClient
+{
+    [GeneratedRegex(@"window\.location\.href\s*=\s*'([^']*)'")]
+    private static partial Regex VideoUrlRedirectRegex();
+
+
+    readonly string hostUrl;
+    readonly string site;
+    readonly ILogger<SerienStreamClient>? logger;
+
+    readonly RequestHelper requestHelper;
+
+    public SerienStreamClient(
+        string hostUrl,
+        string site,
+        bool ignoreCerficiateValidation = false,
+        ILogger<SerienStreamClient>? logger = null)
+    {
+        this.hostUrl = hostUrl;
+        this.site = site;
+        this.logger = logger;
+
+        this.requestHelper = new(ignoreCerficiateValidation, logger);
+
+        logger?.LogInformation("[SerienStreamClient-.ctor] SerienStreamClient has been inizialized.");
+    }
+
+
+    async Task<HtmlNode> GetHtmlRootAsync(
+        string path,
+        CancellationToken cancellationToken = default)
+    {
+        logger?.LogInformation("[SerienStreamClient-GetHtmlRootAsync] Getting HTML document: {path}...", path);
+        string webContent = await requestHelper.GetAndValidateAsync(hostUrl, path, null, cancellationToken);
+
+        HtmlDocument document = new();
+        document.LoadHtml(webContent);
+
+        return document.DocumentNode;
+    }
+
+
+    public async Task<Series> GetSeriesAsync(
+        string title,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync($"{site}/stream/{title.ToRelativePath()}", cancellationToken);
+
+        if (root.Any("//div[contains(@class, 'messageAlert danger')]"))
+            throw new SeriesNotFoundException(title);
+
+        // Parse HTML document into series info
+        logger?.LogInformation("[SerienStreamClient-GetSeriesAsync] Parsing HTML document into series info: {title}...", title);
+
+        string endYearText = root.SelectSingleNodeText("//span[@itemprop='endDate']/a");
+        bool hasMovies = root.SelectSingleNodeTextOrDefault("//div[@id='stream']//ul[1]/li[2]/a") == "Filme";
+
+        return new Series(
+            title: root.SelectSingleNodeText("//div[@class='series-title']/h1"),
+            description: root.SelectSingleNodeAttribute("//p[@class='seri_des']", "data-full-description"),
+            bannerUrl: hostUrl.AddRelativePath(root.SelectSingleNodeAttribute("//div[@class='seriesCoverBox']//img", "data-src")),
+            yearStart: root.SelectSingleNodeText("//span[@itemprop='startDate']/a").ToInt32(),
+            yearEnd: endYearText == "Heute" ? null : endYearText.ToInt32(),
+            directors: root.Select("//li[@itemprop='director']//span[@itemprop='name']", Extensions.GetInnerText),
+            actors: root.Select("//li[@itemprop='actor']//span[@itemprop='name']", Extensions.GetInnerText),
+            creators: root.Select("//li[@itemprop='creator']//span[@itemprop='name']", Extensions.GetInnerText),
+            countriesOfOrigin: root.Select("//li[@itemprop='countryOfOrigin']//span[@itemprop='name']", Extensions.GetInnerText),
+            genres: root.Select("//div[@class='genres']//li/a[@itemprop='genre']", Extensions.GetInnerText),
+            ageRating: root.SelectSingleNodeAttribute("//div[contains(@class, 'fsk')]", "data-fsk").ToInt32(),
+            rating: new Rating(
+                root.SelectSingleNodeText("//div[@itemprop='aggregateRating']//span[@itemprop='ratingValue']").ToInt32(),
+                root.SelectSingleNodeText("//div[@itemprop='aggregateRating']//span[@itemprop='bestRating']").ToInt32(),
+                root.SelectSingleNodeText("//div[@itemprop='aggregateRating']//span[@itemprop='ratingCount']").ToInt32()),
+            imdbUrl: root.SelectSingleNodeAttributeOrDefault("//a[@class='imdb-link']", "href"),
+            trailerUrl: root.SelectSingleNodeAttributeOrDefault("//div[@itemprop='trailer']//a[@itemprop='url']", "href"),
+            hasMovies: hasMovies,
+            seasonsCount: root.SelectSingleNodeAttribute("//meta[@itemprop='numberOfSeasons']", "content").ToInt32() - hasMovies.ToInt32());
+    }
+
+
+    public async Task<Media[]> GetEpisodesAsync(
+        string title,
+        int season,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync($"{site}/stream/{title.ToRelativePath()}/staffel-{season}", cancellationToken);
+
+        if (root.ChildNodes.Count == 0)
+            return season == 0 ? [] : throw new SeasonNotFoundException(title, season);
+        if (root.Any("//div[contains(@class, 'messageAlert danger')]"))
+            throw new SeriesNotFoundException(title);
+
+        // Parse HTML document into series info
+        logger?.LogInformation("[SerienStreamClient-GetEpisodesAsync] Parsing HTML document into media info list: {title}, {season}...", title, season);
+
+        return root.Select("//table[@class='seasonEpisodesList']//tbody//tr", node => new Media(
+            number: node.SelectSingleNodeText(".//td[1]//a")[6..].ToInt32(),
+            title: node.SelectSingleNodeText(".//td[2]/a/strong"),
+            originalTitle: node.SelectSingleNodeText(".//td[2]/a/span"),
+            hosters: node.Select(".//td[3]//i", childNode => childNode.GetAttributeValue("title").ToHoster()),
+            languages: node.Select(".//td[4]//img", childNode => childNode.GetAttributeValue("src").ToMediaLanguage())));
+    }
+
+    public Task<Media[]> GetMoviesAsync(
+        string title,
+        CancellationToken cancellationToken = default) =>
+        GetEpisodesAsync(title, 0, cancellationToken);
+
+
+    public async Task<VideoDetails> GetEpisodeVideoInfoAsync(
+        string title,
+        int number,
+        int season,
+        CancellationToken cancellationToken = default)
+    {
+        // Get HTML doucment
+        HtmlNode root = await GetHtmlRootAsync($"{site}/stream/{title.ToRelativePath()}/staffel-{season}/episode-{number}", cancellationToken);
+
+        if (root.ChildNodes.Count == 0)
+            throw season == 0 ? new MovieNotFoundException(title, number) : new EpisodeNotFoundException(title, season, number);
+        if (root.Any("//div[contains(@class, 'messageAlert danger')]"))
+            throw new SeriesNotFoundException(title);
+        if (!root.Any("//div[contains(@class, 'hosterSiteDirectNav')]"))
+            throw new SeasonNotFoundException(title, season);
+
+        // Parse HTML document into series info
+        logger?.LogInformation("[SerienStreamClient-GetEpisodeVideoInfoAsync] Parsing HTML document into video info: {title}, {number}, {season}...", title, number, season);
+
+        Dictionary<int, MediaLanguage> languageMapping = root.Map("//div[@class='changeLanguageBox']//img", node => (
+            node.GetAttributeValue("data-lang-key").ToInt32(),
+            node.GetAttributeValue("src").ToMediaLanguage()));
+
+        return new VideoDetails(
+            title: root.SelectSingleNodeText("//div[@class='hosterSiteTitle']/h2/span[@class='episodeGermanTitle']"),
+            originalTitle: root.SelectSingleNodeText("//div[@class='hosterSiteTitle']/h2/small[@class='episodeEnglishTitle']"),
+            description: root.SelectSingleNodeText("//div[@class='hosterSiteTitle']/p[@itemprop='description']"),
+            streams: root.Select("//ul[@class='row']/li", node => new VideoStream(
+                redirectId: node.SelectSingleNodeAttribute(".//a[@class='watchEpisode']", "href")[9..],
+                hoster: node.SelectSingleNodeText(".//h4").ToHoster(),
+                language: languageMapping.GetValueOrDefault(node.GetAttributeValue("data-lang-key").ToInt32(), new(Language.Unknown, null)))));
+    }
+
+    public Task<VideoDetails> GetMovieVideoInfoAsync(
+        string title,
+        int number,
+        CancellationToken cancellationToken = default) =>
+        GetEpisodeVideoInfoAsync(title, number, 0, cancellationToken);
+
+
+    public async Task<string> GetVideoUrlAsync(
+        string redirectId,
+        CancellationToken cancellationToken = default)
+    {
+        // Get web content
+        logger?.LogInformation("[SerienStreamClient-GetVideoUrlAsync] Getting video redirect script: {redirectId}...", redirectId);
+        string webContent = await requestHelper.GetAndValidateAsync(hostUrl, $"/redirect/{redirectId}", null, cancellationToken);
+
+        // Extract video url from redirect
+        Match match = VideoUrlRedirectRegex().Match(webContent);
+        if (!match.Success)
+            throw new UrlExtractionFailedException(redirectId);
+
+        return match.Groups[1].Value;
+    }
+}
