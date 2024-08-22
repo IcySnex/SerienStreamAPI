b5ad84866d42bd5fd92bff9962f16306ea5c21d3
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-08-22 21:27:18 +0200

    Added internal utilities

---
+++ SerienStreamAPI/Internal/Extensions.cs
@@ -0,0 +1,144 @@
+﻿using HtmlAgilityPack;
+using SerienStreamAPI.Enums;
+using SerienStreamAPI.Models;
+using System.Xml;
+
+namespace SerienStreamAPI.Internal;
+
+internal static class Extensions
+{
+    public static string AddRelativePath(
+        this string baseUrl,
+        string relativePath) =>
+        $"{baseUrl.Trim('/')}/{relativePath.Trim('/')}";
+
+    public static string ToRelativePath(
+        this string text) =>
+        text.ToLower().Replace(' ', '-');
+
+
+    public static int ToInt32(
+        this string text) =>
+        int.Parse(text);
+
+    public static int ToInt32(
+        this bool boolean) =>
+        boolean ? 1 : 0;
+
+    public static Hoster ToHoster(
+        this string text) =>
+        text.ToLowerInvariant() switch
+        {
+            "voe" => Hoster.VOE,
+            "doodstream" => Hoster.Doodstream,
+            "vidoza" => Hoster.Vidoza,
+            "streamtape" => Hoster.Streamtape,
+            _ => Hoster.Unknown
+        };
+    
+    public static Language ToLanguage(
+        this string text) =>
+        text.ToLowerInvariant() switch
+        {
+            "german" => Language.German,
+            "english" => Language.English,
+            "japanese" => Language.Japanese,
+            _ => Language.Unknown
+        };
+
+    public static MediaLanguage ToMediaLanguage(
+        this string text)
+    {
+        if (text.Length < 15)
+            return new(Language.Unknown, null);
+
+        string[] languageData = text[11..^4].Split('-', StringSplitOptions.RemoveEmptyEntries);
+        return languageData.Length switch
+        {
+            1 => new(languageData[0].ToLanguage(), null),
+            2 => new(languageData[0].ToLanguage(), languageData[1].ToLanguage()),
+            _ => new(Language.Unknown, null)
+        };
+    }
+
+
+    public static string GetInnerText(
+        this HtmlNode? node) =>
+        node?.InnerText.Trim('/') ?? string.Empty;
+
+    public static string GetAttributeValue(
+        this HtmlNode? node,
+        string attributeName) =>
+        node?.GetAttributeValue(attributeName, null).Trim('/') ?? string.Empty;
+
+
+    public static string? SelectSingleNodeTextOrDefault(
+        this HtmlNode node,
+        string xpath)
+    {
+        HtmlNode? result = node.SelectSingleNode(xpath);
+        return result.GetInnerText();
+    }
+    
+    public static string? SelectSingleNodeAttributeOrDefault(
+        this HtmlNode node,
+        string xpath,
+        string attributeName)
+    {
+        HtmlNode? result = node.SelectSingleNode(xpath);
+        return result.GetAttributeValue(attributeName);
+    }
+
+
+    public static string SelectSingleNodeText(
+        this HtmlNode node,
+        string xpath) =>
+        node.SelectSingleNodeTextOrDefault(xpath) ?? throw new NodeNotFoundException($"Could not find node: \"{xpath}\".");
+
+    public static string SelectSingleNodeAttribute(
+        this HtmlNode node,
+        string xpath,
+        string attributeName) =>
+        node.SelectSingleNodeAttributeOrDefault(xpath, attributeName) ?? throw new NodeAttributeNotFoundException($"Could not find node or attribute: \"{xpath}\" - \"{attributeName}\".");
+
+
+    public static bool Any(
+        this HtmlNode node,
+        string xpath) =>
+        node.SelectSingleNode(xpath) is not null;
+
+    public static T[] Select<T>(
+        this HtmlNode node,
+        string xpath,
+        Func<HtmlNode, T> selector)
+    {
+        HtmlNodeCollection? nodes = node.SelectNodes(xpath);
+        if (nodes is null)
+            return [];
+
+        T[] result = new T[nodes.Count];
+        for (int i = 0; i < nodes.Count; i++)
+            result[i] = selector(nodes[i]);
+
+        return result;
+    }
+    
+    public static Dictionary<TKey, TValue> Map<TKey, TValue>(
+        this HtmlNode node,
+        string xpath,
+        Func<HtmlNode, (TKey, TValue)> selector) where TKey : notnull
+    {
+        HtmlNodeCollection? nodes = node.SelectNodes(xpath);
+        if (nodes is null)
+            return [];
+
+        Dictionary<TKey, TValue> result = [];
+        foreach (HtmlNode childNode in nodes)
+        {
+            var (key, value) = selector(childNode);
+            result[key] = value;
+        }
+
+        return result;
+    }
+}
---
+++ SerienStreamAPI/Internal/RequestHelper.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SerienStreamAPI.Internal;
+
+/// <summary>
+/// Helper which handles all HTTP requests
+/// </summary>
+internal class RequestHelper
+{
+    readonly ILogger? logger;
+
+    readonly HttpClient httpClient;
+
+    /// <summary>
+    /// Creates a new request helper with extendended logging functions
+    /// </summary>
+    /// <param name="logger">The optional logger used for logging</param>
+    public RequestHelper(
+        bool ignoreCerficiateValidation = false,
+        ILogger? logger = null)
+    {
+        this.logger = logger;
+
+        HttpClientHandler handler = new();
+        if (ignoreCerficiateValidation)
+            handler.ServerCertificateCustomValidationCallback += (s, cert, chain, errors) => true;
+        httpClient = new(handler);
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+
+        logger?.LogInformation($"[RequestHelper-.ctor] RequestHelper with extendended logging functions has been initialized.");
+    }
+
+
+    /// <summary>
+    /// Sends a new GET request to the given uri with the parameters
+    /// </summary>
+    /// <param name="url">The uri the request should be made to</param>
+    /// <param name="path">The relatvie path of the request url</param>
+    /// <param name="parameters">The query parameters which should be added</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the action</param>
+    /// <exception cref="System.InvalidOperationException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Net.Http.HttpRequestException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Threading.Tasks.TaskCanceledException">Occurs when The task was cancelled</exception>
+    /// <returns>The HTTP response message</returns>
+    public Task<HttpResponseMessage> GetAsync(
+        string url,
+        string? path = null,
+        (string key, string value)[]? headers = null,
+        CancellationToken cancellationToken = default)
+    {
+        UriBuilder builder = new(url);
+        if (path is not null)
+            builder.Path = path;
+
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Get,
+            RequestUri = builder.Uri
+        };
+        if (headers is not null)
+            foreach ((string key, string value) in headers)
+                request.Headers.Add(key, value);
+
+        logger?.LogInformation("[RequestHelper-GetAsync] Sending HTTP reuqest. GET: {url}.", url);
+        return httpClient.SendAsync(request, cancellationToken);
+    }
+
+    /// <summary>
+    /// Sends a new GET request to the given uri with the parameters and validates it
+    /// </summary>
+    /// <param name="uri">The uri the request should be made to</param>
+    /// <param name="path">The relatvie path of the request url</param>
+    /// <param name="parameters">The query parameters which should be added</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the action</param>
+    /// <exception cref="System.InvalidOperationException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Net.Http.HttpRequestException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Threading.Tasks.TaskCanceledException">Occurs when The task was cancelled</exception>
+    /// <returns>The validated HTTP response data</returns>
+    public async Task<string> GetAndValidateAsync(
+        string uri,
+        string? path = null,
+        (string key, string value)[]? headers = null,
+        CancellationToken cancellationToken = default)
+    {
+        HttpResponseMessage httpResponse = await GetAsync(uri, path, headers, cancellationToken).ConfigureAwait(false);
+
+        logger?.LogInformation($"[RequestHelper-GetAndValidateAsync] Parsing HTTP response.");
+        string httpResponseData = await httpResponse.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            logger?.LogError("[RequestHelper-GetAndValidateAsync] HTTP request failed. Statuscode: {statusCode}.", httpResponse.StatusCode);
+            throw new HttpRequestException($"HTTP request failed. StatusCode: {httpResponse.StatusCode}.", new(httpResponseData));
+        }
+
+        return httpResponseData;
+    }
+}
